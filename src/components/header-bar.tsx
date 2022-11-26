88f54f11a3ec7a29e1ed2253c4f326f5f3077031
M-Rebbi <rebmus96@gmail.com>
2022-11-26 12:06:34 +0400

    searchbar styling

--- src/components/header-bar.tsx
+++ src/components/header-bar.tsx
@@ -1,10 +1,15 @@
 import React, {useCallback, useRef} from 'react';
 import {TextInput as RNTextInput} from 'react-native';
-import {Bar, TextInput} from '@/atoms';
+import {Bar, TextInput, TouchableOpacity} from '@/atoms';
 import AnimatedBox, {AnimatedBoxProps} from '@/atoms/animated-box';
 import {searchInputHasFocusAtom, searchQueryAtom} from '@/states/search-bar';
 import {useAtom} from 'jotai';
 import HeaderBarLeftButton from './header-bar-left-button';
+import FeatherIcon from './icon';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {Theme} from '@/themes';
+import {useTheme} from '@shopify/restyle';
+import {useAnimatedStyle, withTiming} from 'react-native-reanimated';
 
 type Props = AnimatedBoxProps & {
   onSidebarToggle: () => any;
@@ -12,7 +17,9 @@ type Props = AnimatedBoxProps & {
 
 const HeaderBar: React.FC<Props> = props => {
   const {onSidebarToggle, ...rest} = props;
-  const [searcQuery, setSearchQuery] = useAtom(searchQueryAtom);
+  const safeAreaInsets = useSafeAreaInsets();
+  const theme = useTheme<Theme>();
+  const [searchQuery, setSearchQuery] = useAtom(searchQueryAtom);
   const [searchInputHasFocus, setSearchInputHasFocus] = useAtom(
     searchInputHasFocusAtom,
   );
@@ -26,6 +33,10 @@ const HeaderBar: React.FC<Props> = props => {
     setSearchInputHasFocus(false);
   };
 
+  const handleClearButtonPress = () => {
+    setSearchQuery('');
+  };
+
   const handleLeftButtonPress = useCallback(() => {
     if (searchInputHasFocus) {
       refSearchInput.current?.blur();
@@ -34,16 +45,43 @@ const HeaderBar: React.FC<Props> = props => {
     }
   }, [searchInputHasFocus, onSidebarToggle]);
 
+  const safeAreaStyle = useAnimatedStyle(
+    () => ({
+      opacity: withTiming(searchInputHasFocus ? 1 : 0),
+    }),
+    [searchInputHasFocus],
+  );
+
+  const barStyle = useAnimatedStyle(
+    () => ({
+      marginHorizontal: withTiming(searchInputHasFocus ? 0 : theme.spacing.lg),
+      borderRadius: withTiming(searchInputHasFocus ? 0 : theme.borderRadii.md, {
+        duration: 600,
+      }),
+    }),
+    [searchInputHasFocus],
+  );
+
   return (
     <AnimatedBox position="absolute" top={0} left={0} right={0} {...rest}>
-      <Bar
-        variant="headerBar"
+      <AnimatedBox
+        position="absolute"
+        top={-safeAreaInsets.top}
+        left={0}
+        right={0}
+        bottom={0}
+        bg="$headerBarBackground"
+        style={safeAreaStyle}
+      />
+      <AnimatedBox
         flexDirection="row"
         alignItems="center"
         mx="lg"
         my="md"
         px="sm"
-        minHeight={44}>
+        minHeight={44}
+        bg="$headerBarBackground"
+        style={barStyle}>
         <HeaderBarLeftButton
           onPress={handleLeftButtonPress}
           backButtonVisible={searchInputHasFocus}
@@ -57,12 +95,21 @@ const HeaderBar: React.FC<Props> = props => {
           color="$foreground"
           placeholder="Search Notes"
           placeholderColor="$fieldInputPlaceholderTextColor"
-          value={searcQuery}
+          value={searchQuery}
           onFocus={handleSearchInputFocus}
           onBlur={handleSearchInputBlur}
           onChangeText={setSearchQuery}
         />
-      </Bar>
+        {searchQuery.length > 0 && (
+          <TouchableOpacity
+            m="xs"
+            p="xs"
+            rippleBorderless
+            onPress={handleClearButtonPress}>
+            <FeatherIcon name="x" size={22} />
+          </TouchableOpacity>
+        )}
+      </AnimatedBox>
     </AnimatedBox>
   );
 };
